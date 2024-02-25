2f7a7b9eb46d0094b2cd6296bc87727a27f5322c
DESKTOP-1DUM6AO\aaron <aaronballes11@svalero.com>
2024-02-25 21:07:42 +0100

    Avanzando en otros controllers y salida de datos

--- Data/ISesionAsientoRepository.cs
+++ Data/ISesionAsientoRepository.cs
@@ -6,15 +6,11 @@ namespace TeatroApi.Data
     {
         void AddSesionAsiento(SesionAsiento sesionAsiento);
 
-        //De momento hacemos que no salga la compra por id porque no tiene PK        
-        // Compra GetCompra(int compraId);
-        
+
         List<SesionAsiento> GetSesionAsientosByAsiento(int asientoId);
         List<SesionAsiento> GetSesionAsientosBySesion(int sesionId);
         void UpdateSesionAsiento(SesionAsiento sesionAsiento);
 
-        //De momento hacemos que no salga la compra por id porque no tiene PK        
-        // void RemoveCompra(int compraId);
 
         void SaveChanges();
         List<SesionAsiento> GetSesionAsientos();
