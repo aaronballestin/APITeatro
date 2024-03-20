1c2bd68b90ea80c459c6a2152decb7abecba5019
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2024-03-20 19:38:58 +0100

    Added gitignore && Added Precio in SesionDTO

--- Models/Sesiones/SesionIntranetDTO.cs
+++ Models/Sesiones/SesionIntranetDTO.cs
@@ -7,12 +7,15 @@ namespace TeatroApi.Models
         public string nombreObra {get; set;}
         public DateTime date {get; set;}
 
+        public double precio {get;set;}
+
         public SesionIntranetDTO(int sesion, int obra, int sala, DateTime horario, string nombreObra, double precio){
             this.id = sesion;
             this.obraId = obra;
             this.salaId = sala;
             this.date = horario;
             this.nombreObra = nombreObra;
+            this.precio = precio;
         }
 
           
